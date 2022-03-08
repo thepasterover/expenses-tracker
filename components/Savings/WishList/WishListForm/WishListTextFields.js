bf01a5b7001eebde083fb3331714bf03bf353986
thepasterover <caped_crusad3r@protonmail.com>
2022-03-08 22:47:09 +0530

    Fix Savings Amount Error in Expenses Tracker

--- components/Savings/WishList/WishListForm/WishListTextFields.js
+++ components/Savings/WishList/WishListForm/WishListTextFields.js
@@ -1,254 +1,329 @@
 import React, { useState } from 'react'
 
-import { 
-    Box, 
-    TextField, 
-    InputAdornment, 
-    FormControl, 
-    OutlinedInput, 
-    InputLabel ,
-    Button,
-    FormHelperText
+import {
+	Box,
+	TextField,
+	InputAdornment,
+	FormControl,
+	OutlinedInput,
+	InputLabel,
+	Button,
+	FormHelperText
 } from '@material-ui/core'
 import { toast } from 'react-toastify'
 
+const WishListTextFields = ({
+	setOpen,
+	type,
+	add,
+	data,
+	edit,
+	change,
+	status,
+	setStateStatus,
+	del
+}) => {
+	const [subject, setSubject] = useState(data.subject || '')
+	const [totalAmount, setTotalAmount] = useState(data.totalAmount || '')
+	const [savingsAmount, setSavingsAmount] = useState(data.savingsAmount || '')
+	const [currentAmount, setCurrentAmount] = useState(data.currentAmount || '')
+	const [disabled, setDisabled] = useState(false)
+	const [errors, setErrors] = useState({
+		subjectError: '',
+		totalAmountError: '',
+		savingsAmountError: '',
+		currentAmountError: ''
+	})
 
-const WishListTextFields = ({setOpen, type, add, data, edit, change, status, setStateStatus, del}) => {
+	const validateSubject = (subject) => {
+		if (subject === '') {
+			setErrors({ ...errors, subjectError: 'Subject is required!' })
+			return
+		} else if (subject.length <= 3) {
+			setErrors({
+				...errors,
+				subjectError: 'Subject must be greater than 3 characters!'
+			})
+			return
+		} else if (subject.length >= 300) {
+			setErrors({
+				...errors,
+				subjectError: 'Subject must be less than 300 characters!'
+			})
+			return
+		} else {
+			setErrors({ ...errors, subjectError: '' })
+			return
+		}
+	}
 
-    const [ subject, setSubject ] = useState( data.subject || '')
-    const [ totalAmount, setTotalAmount ] = useState(data.totalAmount || '')
-    const [ savingsAmount, setSavingsAmount ] = useState(data.savingsAmount || '')
-    const [ currentAmount, setCurrentAmount ] = useState(data.currentAmount || '')
-    const [ disabled, setDisabled ] = useState(false)
-    const [ errors, setErrors ] = useState({
-        subjectError: '',
-        totalAmountError: '',
-        savingsAmountError: '',
-        currentAmountError: ''
-    })
-    
-    const validateSubject = (subject) => {
-        if(subject === ''){
-            setErrors({...errors, 
-                subjectError: 'Subject is required!'
-            })
-            return
-        } else if (subject.length <= 3) {
-            setErrors({...errors, 
-                subjectError: 'Subject must be greater than 3 characters!'
-            })
-            return
-        } else if (subject.length >= 300) {
-            setErrors({...errors, 
-                subjectError: 'Subject must be less than 300 characters!'
-            })
-            return
-        } else {
-            setErrors({...errors, 
-                subjectError: ''
-            })
-            return
-        }
-    }
+	const validateTotalAmount = (totalAmount) => {
+		if (totalAmount === '') {
+			setErrors({
+				...errors,
+				totalAmountError: 'Total Amount is required!'
+			})
+			return
+		} else if (isNaN(totalAmount)) {
+			setErrors({
+				...errors,
+				totalAmountError: 'Total Amount must be a Number!'
+			})
+			return
+		} else if (totalAmount <= 0) {
+			setErrors({
+				...errors,
+				totalAmountError:
+					'Total Amount cannot be less than or equal to 0!'
+			})
+			return
+		} else {
+			setErrors({ ...errors, totalAmountError: '' })
+			return
+		}
+	}
 
-    const validateTotalAmount = (totalAmount) => {
-        if(totalAmount === ''){
-            setErrors({...errors, 
-                totalAmountError: 'Total Amount is required!'
-            })
-            return
-        } else if (isNaN(totalAmount)) {
-            setErrors({...errors, 
-                totalAmountError: 'Total Amount must be a Number!'
-            })
-            return
-        } else if(totalAmount <= 0) {
-            setErrors({...errors, 
-                totalAmountError: 'Total Amount cannot be less than or equal to 0!'
-            })
-            return
-        } else {
-            setErrors({...errors, 
-                totalAmountError: ''
-            })
-            return
-        }
-    }
+	const validateSavingsAmount = (savingsAmount) => {
+		if (savingsAmount === '') {
+			setErrors({
+				...errors,
+				savingsAmountError: 'Savings Amount is required!'
+			})
+			return
+		} else if (isNaN(savingsAmount)) {
+			setErrors({
+				...errors,
+				savingsAmountError: 'Savings Amount must be a Number!'
+			})
+			return
+		} else if (savingsAmount <= 0) {
+			setErrors({
+				...errors,
+				savingsAmountError:
+					'Savings Amount cannot be less than or equal to 0!'
+			})
+			return
+		} else {
+			setErrors({ ...errors, savingsAmountError: '' })
+			return
+		}
+	}
 
-    const validateSavingsAmount = (savingsAmount) =>{
-        if(savingsAmount === ''){
-            setErrors({...errors, 
-                savingsAmountError: 'Savings Amount is required!'
-            })
-            return
-        } else if (isNaN(savingsAmount)) {
-            setErrors({...errors, 
-                savingsAmountError: 'Savings Amount must be a Number!'
-            })
-            return
-        } else if(savingsAmount <= 0) {
-            setErrors({...errors, 
-                savingsAmountError: 'Savings Amount cannot be less than or equal to 0!'
-            })
-            return
-        } else {
-            setErrors({...errors, 
-                savingsAmountError: ''
-            })
-            return
-        }
-    }
+	const validateCurrentAmount = (currentAmount) => {
+		if (currentAmount === '') {
+			setErrors({
+				...errors,
+				currentAmountError: 'Current Amount is required!'
+			})
+			return
+		} else if (isNaN(currentAmount)) {
+			setErrors({
+				...errors,
+				currentAmountError: 'Current Amount must be a Number!'
+			})
+			return
+		} else if (savingsAmount <= 0) {
+			setErrors({
+				...errors,
+				currentAmountError:
+					'Current Amount cannot be less than or equal to 0!'
+			})
+			return
+		} else {
+			setErrors({ ...errors, currentAmountError: '' })
+			return
+		}
+	}
 
-    const validateCurrentAmount = (currentAmount) => {
-        if(currentAmount === ''){
-            setErrors({...errors, 
-                currentAmountError: 'Current Amount is required!'
-            })
-            return
-        } else if (isNaN(currentAmount)) {
-            setErrors({...errors, 
-                currentAmountError: 'Current Amount must be a Number!'
-            })
-            return
-        } else if(savingsAmount <= 0) {
-            setErrors({...errors, 
-                currentAmountError: 'Current Amount cannot be less than or equal to 0!'
-            })
-            return
-        } else {
-            setErrors({...errors, 
-                currentAmountError: ''
-            })
-            return
-        }
-    }
+	const onClickAdd = () => {
+		if (subject === '' || totalAmount === '' || savingsAmount === '') {
+			toast.error('Please fill up the form properly!')
+		} else if (
+			errors.subjectError !== '' ||
+			errors.totalAmountError !== '' ||
+			errors?.savingsAmountError !== ''
+		) {
+			toast.error('Please fix the errors in the form!')
+		} else {
+			setOpen(false)
+			add(subject, totalAmount, savingsAmount)
+			setSubject('')
+			setTotalAmount('')
+			setSavingsAmount('')
+		}
+	}
 
-    const onClickAdd = () => {
-        if(subject === '' || totalAmount === '' || savingsAmount === ''){
-            toast.error('Please fill up the form properly!')
-        } else if(errors.subjectError !== '' || errors.totalAmountError !== '' || errors.savingAmountError !== ''){
-            toast.error('Please fix the errors in the form!')
-        } else {
-            setOpen(false)
-            add(subject, totalAmount, savingsAmount)
-            setSubject('')
-            setTotalAmount('')
-            setSavingsAmount('')
-        }
-    }
+	const onClickEdit = () => {
+		if (
+			subject === '' ||
+			totalAmount === '' ||
+			savingsAmount === '' ||
+			currentAmount === ''
+		) {
+			toast.error('Please fill up the form properly!')
+		} else if (
+			errors.subjectError !== '' ||
+			errors.totalAmountError !== '' ||
+			errors.currentAmountError !== ''
+		) {
+			toast.error('Please fix the errors in the form!')
+		} else {
+			setOpen(false)
+			edit(data.id, subject, totalAmount, savingsAmount, currentAmount)
+		}
+	}
 
-    const onClickEdit = () => {
-        if(subject === '' || totalAmount === '' || savingsAmount === '' || currentAmount === ''){
-            toast.error('Please fill up the form properly!')
-        } else if(errors.subjectError !== '' || errors.totalAmountError !== '' || errors.currentAmountError !== ''){
-            console.log((errors.savingAmountError !== '' ))
-            toast.error('Please fix the errors in the form!')
-        } else {
-            setOpen(false)
-            edit(data.id, subject, totalAmount, savingsAmount, currentAmount)
-        }
-        
-    }
+	const onClickChange = () => {
+		setDisabled(true)
+		change(data.id)
+		setStateStatus(!status)
+		setTimeout(() => {
+			setDisabled(false)
+		}, 2000)
+	}
 
-    const onClickChange = () => {
-        setDisabled(true)
-        change(data.id)
-        setStateStatus(!status)
-        setTimeout(() => {
-            setDisabled(false)
-        }, 2000)
-    }
+	let viewBtns
+	type.toLowerCase() === 'add'
+		? (viewBtns = (
+				<Box>
+					<Button
+						variant='contained'
+						color='primary'
+						onClick={onClickAdd}
+					>
+						Add
+					</Button>
+				</Box>
+		  ))
+		: (viewBtns = (
+				<>
+					<Box>
+						<Button
+							variant='contained'
+							style={{
+								color: '#fff',
+								backgroundColor: '#848E98'
+							}}
+							onClick={onClickChange}
+							disabled={disabled}
+						>
+							{status ? 'Hold' : 'Unhold'}
+						</Button>
+					</Box>
+					<Box>
+						<Button
+							variant='contained'
+							color='primary'
+							onClick={onClickEdit}
+						>
+							Edit
+						</Button>
+					</Box>
+					<Box>
+						<Button
+							variant='contained'
+							style={{
+								color: '#fff',
+								backgroundColor: '#c82333'
+							}}
+							onClick={() => {
+								setOpen(false)
+								del(data.id)
+							}}
+						>
+							Delete
+						</Button>
+					</Box>
+				</>
+		  ))
 
-    let viewBtns
-    type.toLowerCase() === 'add' ? 
-    viewBtns = (
-        <Box>
-            <Button variant="contained" color="primary" onClick={onClickAdd}>
-                Add
-            </Button>
-        </Box>
-    ) :
-    viewBtns = (
-        <>
-            <Box>
-                <Button variant="contained" style={{color: '#fff', backgroundColor: '#848E98'}} onClick={onClickChange} disabled={disabled}>
-                    {status ? 'Hold' : 'Unhold'}
-                </Button>
-            </Box>
-            <Box>
-                <Button variant="contained" color="primary" onClick={onClickEdit}>
-                    Edit
-                </Button>
-            </Box>
-            <Box>
-                <Button variant="contained" style={{color: '#fff', backgroundColor: '#c82333'}} onClick={() => { setOpen(false); del(data.id) }}>
-                    Delete
-                </Button>
-            </Box>
-        </>
-    )
+	return (
+		<>
+			<Box mt={2}>
+				<TextField
+					color='primary'
+					variant='outlined'
+					fullWidth
+					label='Subject'
+					value={subject || ''}
+					onChange={(event) => {
+						setSubject(event.target.value)
+						validateSubject(event.target.value)
+					}}
+					error={errors.subjectError !== ''}
+					helperText={errors.subjectError}
+				/>
+			</Box>
+			<Box mt={2}>
+				<TextField
+					color='primary'
+					variant='outlined'
+					fullWidth
+					label='Total Amount'
+					value={totalAmount || ''}
+					onChange={(event) => {
+						setTotalAmount(event.target.value)
+						validateTotalAmount(event.target.value)
+					}}
+					error={errors.totalAmountError !== ''}
+					helperText={errors.totalAmountError}
+				/>
+			</Box>
+			<Box mt={2}>
+				<FormControl
+					variant='outlined'
+					fullWidth
+					error={errors.savingsAmountError !== ''}
+				>
+					<InputLabel htmlFor='outlined-adornment-savings-amount'>
+						Savings Amount
+					</InputLabel>
+					<OutlinedInput
+						id='outlined-adornment-password'
+						label='Savings Amount'
+						fullWidth
+						endAdornment={
+							<InputAdornment position='end'>
+								/ month
+							</InputAdornment>
+						}
+						value={savingsAmount || ''}
+						onChange={(event) => {
+							setSavingsAmount(event.target.value)
+							validateSavingsAmount(event.target.value)
+						}}
+					/>
+				</FormControl>
+				<FormHelperText
+					id='outlined-adornment-savings-amount'
+					style={{ color: 'red' }}
+				>
+					{errors.savingsAmountError}
+				</FormHelperText>
+			</Box>
+			{type.toLowerCase() === 'edit' && (
+				<Box mt={2}>
+					<TextField
+						color='primary'
+						variant='outlined'
+						fullWidth
+						label='Current Amount'
+						value={currentAmount || ''}
+						onChange={(event) => {
+							setCurrentAmount(event.target.value)
+							validateCurrentAmount(event.target.value)
+						}}
+						error={errors.currentAmountError !== ''}
+						helperText={errors.currentAmountError}
+					/>
+				</Box>
+			)}
 
-    return (
-        <>
-            <Box mt={2}>
-                <TextField
-                color="primary"
-                variant="outlined"
-                fullWidth
-                label="Subject"
-                value={subject || ""}
-                onChange={event => {setSubject(event.target.value); validateSubject(event.target.value)}}
-                error={errors.subjectError !== ''}
-                helperText={errors.subjectError}
-                />
-            </Box>
-            <Box mt={2}>
-                <TextField
-                color="primary"
-                variant="outlined"
-                fullWidth
-                label="Total Amount"
-                value={totalAmount || ""}
-                onChange={event => {setTotalAmount(event.target.value); validateTotalAmount(event.target.value)}}
-                error={errors.totalAmountError !== ''}
-                helperText={errors.totalAmountError}
-                />
-            </Box>
-            <Box mt={2}>
-                <FormControl variant="outlined" fullWidth error={errors.savingsAmountError !== ''}>
-                    <InputLabel htmlFor="outlined-adornment-savings-amount">Savings Amount</InputLabel>
-                    <OutlinedInput 
-                        id="outlined-adornment-password"
-                        label="Savings Amount"
-                        fullWidth
-                        endAdornment={
-                            <InputAdornment position="end">/ month</InputAdornment>
-                        }
-                        value={savingsAmount || ""}
-                        onChange={event => {setSavingsAmount(event.target.value); validateSavingsAmount(event.target.value)}}
-                    />
-                </FormControl>
-                <FormHelperText id='outlined-adornment-savings-amount' style={{color :'red'}}>{errors.savingsAmountError}</FormHelperText>
-            </Box>
-            { type.toLowerCase() === 'edit' &&  
-            <Box mt={2}>
-                <TextField
-                color="primary"
-                variant="outlined"
-                fullWidth
-                label="Current Amount"
-                value={currentAmount || ""}
-                onChange={event => {setCurrentAmount(event.target.value); validateCurrentAmount(event.target.value)}}
-                error={errors.currentAmountError !== ''}
-                helperText={errors.currentAmountError}
-                />
-            </Box>}
-            
-            <Box display="flex" justifyContent="space-around" pt={5}>
-                {viewBtns}
-            </Box>
-        </>
-    )
+			<Box display='flex' justifyContent='space-around' pt={5}>
+				{viewBtns}
+			</Box>
+		</>
+	)
 }
 
 export default WishListTextFields
--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 import AppBar from '@components/Navigation/MyAppBar'
 import Drawer from '@components/Navigation/MyDrawer'
@@ -7,7 +7,7 @@ import BottomDrawer from '@components/Navigation/BottomDrawer'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/client'
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 
 import { connect } from 'react-redux'
@@ -15,100 +15,93 @@ import { bindActionCreators } from 'redux'
 
 import { fetchCategories } from '../store/category/action'
 import { setNav } from '../store/nav/action'
-import { toast } from 'react-toastify';
-
+import { toast } from 'react-toastify'
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-    },
-    toolbar: theme.mixins.toolbar,
-    content: {
-      flexGrow: 1,
-      backgroundColor: theme.palette.background.default,
-      padding: theme.spacing(3),
-      [theme.breakpoints.down('sm')]:{
-        paddingBottom: theme.spacing(10)
-      },
-    },
-}));
+	root: {
+		display: 'flex'
+	},
+	toolbar: theme.mixins.toolbar,
+	content: {
+		flexGrow: 1,
+		backgroundColor: theme.palette.background.default,
+		padding: theme.spacing(3),
+		[theme.breakpoints.down('sm')]: {
+			paddingBottom: theme.spacing(10)
+		}
+	}
+}))
 
 const navLinks = [
-  {name: 'Dashboard', link: '/dashboard', icon: 'dashboard'}, 
-  {name: 'Transactions', link: '/transactions', icon: 'credit_card'}, 
-  {name: 'Savings', link: '/savings', icon: 'savings'}, 
-  {name: 'Profile', link: '/profile', icon: 'portrait'}
+	{ name: 'Dashboard', link: '/dashboard', icon: 'dashboard' },
+	{ name: 'Transactions', link: '/transactions', icon: 'credit_card' },
+	{ name: 'Savings', link: '/savings', icon: 'savings' },
+	{ name: 'Profile', link: '/profile', icon: 'portrait' }
 ]
 
+const Layout = ({ children, fetchCategories, title, index, setNav }) => {
+	const router = useRouter()
+	const classes = useStyles()
+	const [session, loading] = useSession()
+	const selectedIndex =
+		index || navLinks.findIndex((l) => l.link === router.pathname)
+	const selectedTitle =
+		title ||
+		navLinks.find((l) => l.link === router.pathname)?.name ||
+		'Loading'
+	const avatarUrl = session?.user?.avatar?.url
+		? session.user.avatar.url
+		: '/public/images/avatars/default.jpg'
 
-const Layout = ({children, fetchCategories, title, index, setNav}) => {
-    const router = useRouter()
-    const classes = useStyles();
-    const [ session, loading ] = useSession()
-    const selectedIndex = index || navLinks.findIndex(l => l.link === router.pathname)
-    const selectedTitle = title || navLinks.find(l => l.link === router.pathname)?.name || 'Error'
-    const avatarUrl = session?.user?.avatar?.url ? session.user.avatar.url : '/public/images/avatars/default.jpg'
+	useEffect(() => {
+		try {
+			fetchCategories()
+		} catch (err) {
+			if (session) {
+				toast.error(
+					'Categories could not be loaded. Please refresh the page or check your internet connection!'
+				)
+			}
+		}
+	}, [])
 
-    useEffect(() => {
-      try {
-          fetchCategories()
-      } catch(err) {
-        if(session){
-          toast.error('Categories could not be loaded. Please refresh the page or check your internet connection!')
-        }
-      }
-    }, [])
+	if (loading) return <p>loading</p>
 
+	if (!loading && !session) {
+		return <div>{children}</div>
+	}
 
-    if (loading) return <p>loading</p>
-
-    
-    if(!loading && !session){
-      return (
-        <div>
-            {children}
-        </div>
-      )
-    }
-
-    return (
-        <div className={classes.root}>
-          
-        <AppBar 
-        title={selectedTitle}
-        avatar={avatarUrl}
-        setNav={setNav}
-        />
-        <Drawer 
-        links={navLinks}
-        selectedIndex={selectedIndex}
-        setNav={setNav}
-        />
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
-              <Container>
-                {children}
-              </Container>
-        </main>
-      <BottomDrawer 
-      links={navLinks}
-      selectedIndex={selectedIndex}
-      setNav={setNav}
-      />
-    </div>
-    )
+	return (
+		<div className={classes.root}>
+			<AppBar title={selectedTitle} avatar={avatarUrl} setNav={setNav} />
+			<Drawer
+				links={navLinks}
+				selectedIndex={selectedIndex}
+				setNav={setNav}
+			/>
+			<main className={classes.content}>
+				<div className={classes.toolbar} />
+				<Container>{children}</Container>
+			</main>
+			<BottomDrawer
+				links={navLinks}
+				selectedIndex={selectedIndex}
+				setNav={setNav}
+			/>
+		</div>
+	)
 }
 
 const mapStateToProps = (state) => ({
-  title: state.nav.title,
-  index: state.nav.index
+	title: state.nav.title,
+	index: state.nav.index
 })
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchCategories: bindActionCreators(fetchCategories, dispatch),
-    setNav: bindActionCreators(setNav, dispatch),
-  }
+	return {
+		fetchCategories: bindActionCreators(fetchCategories, dispatch),
+		setNav: bindActionCreators(setNav, dispatch)
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Layout)
